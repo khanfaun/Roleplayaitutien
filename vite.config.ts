48f8dbdf97d3d059f2a526ca6921106015856a3f
khanfaun <khangphan1612@gmail.com>
2025-09-19 16:50:10 +0700

    feat: Update path aliases for Vite and TSConfig
    This commit updates the path aliases in `vite.config.ts` and `tsconfig.json` to correctly resolve module imports. It replaces `fileURLToPath` with `path.resolve(__dirname, './')` for better compatibility with ES modules in Vite and simplifies the `tsconfig.json` path mapping to resolve directly from the root.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { URL, fileURLToPath } from 'url'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // FIX: Replace __dirname with a method compatible with ES modules, as recommended by Vite.
-      '@': fileURLToPath(new URL('./src', import.meta.url)), // trỏ thẳng đến root
+      '@': path.resolve(__dirname, './'), // root
     },
   },
 })
